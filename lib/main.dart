69fae33ebf11f1a8ea31729bcc07fb73875d3b73
GayanPerera95 <62623357+GayanPerera95@users.noreply.github.com>
2021-04-15 23:57:44 +0530

    Edit

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,23 @@ void main() =>
       appBar: AppBar(
         title: Text('My first App'),
         centerTitle: true,
+        backgroundColor: Colors.red[600] ,
       ),
       body: Center(
-        child: Text('Hello Gayan'),
+        child: Text('Hello Gayan',
+        style : TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[900]
+
+        ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: (){},
         child: Text('Click'),
+        backgroundColor: Colors.red[600],
       ),
     ),
 
